2b38f151f500f1a3f2fcd7de6c0c87b09689599c
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2020-08-18 17:28:25 +0530

    Delete Sample Data Preprocessing.py

--- TensorFlow Models/EEG/Sample Data Preprocessing.py
+++
@@ -1,5 +0,0 @@
-file_input = open('D:\Downloads\IN.txt', 'r')
-file_output = open('D:\Competitions\Stroke Project\Code\EEG\Sample EEG Data 3.txt', 'a')
-for i in range(1000):
-    x = file_input.readline()
-    file_output.write('\n'.join((k.strip() for k in x.split(',')[6:])) + '\n')
