26e44fcb9dc95414cf596f499ec2ad40abf2bc24
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2020-08-20 08:38:43 +0530

    Add files via upload

--- Main App/app/src/main/java/com/example/strokeapp/tests/FacialDroopTest.java
+++ Main App/app/src/main/java/com/example/strokeapp/tests/FacialDroopTest.java
@@ -1,25 +1,46 @@
 package com.example.strokeapp.tests;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.strokeapp.R;
+import com.example.strokeapp.TensorflowModelFragment;
 
 public class FacialDroopTest extends AppCompatActivity {
     //UI elements
-    private TextView results;
-    private ImageView imageView;
+    private TextView result;
+    private TensorflowModelFragment tfModelFragment;
 
+    private float[][] results = new float[1][2];
+
+    private Runnable postAnalysis = new Runnable() {
+        @Override
+        public void run() {
+            result.setText(String.format(getString(R.string.results_prompt), results[0][0]));
+        }
+    };
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facial_droop_test);
 
         //Initialize UI
-        results = findViewById(R.id.results);
-        imageView = findViewById(R.id.display_image_view);
+        result = findViewById(R.id.results);
+        result.setText("Results: Stroke probability: ");
+
+        tfModelFragment = (TensorflowModelFragment) getSupportFragmentManager().findFragmentById(R.id.tensorflow_fragment);
+        tfModelFragment.setup("facial_droop_model.tflite", false, 440, 440, results, postAnalysis);
+    }
+
+    public void captureImage(View view) {
+        tfModelFragment.doInference();
     }
 }
